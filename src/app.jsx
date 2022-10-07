4f4678d0a24c03fd0b8ce6d6821274db32f58ba8
ymun1993 <ymun76@gmail.com>
2022-10-07 11:51:43 -0400

    add responsive mode and clean up

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
 import styles from "./app.module.css";
+import VideoDetail from "./components/video_detail/video_detail";
 
 const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
-  const search = (query) => {
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
   };
+  const search = useCallback(
+    (query) => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then((videos) => setVideos(videos));
+    },
+    [youtube]
+  );
 
   useEffect(() => {
     youtube
       .mostPopular() //
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.container}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app.jsx";
 import Youtube from "./Service/youtube";
+import axios from "axios";
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+
+const youtube = new Youtube(httpClient);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
